815b18264c7525628d3bf256ef5972c5f7361a10
Abhishek Gupta <ag251994@gmail.com>
2020-05-17 22:48:45 +0530

    Grid util added

---
+++ src/utils/grid/index.js
@@ -0,0 +1,14 @@
+export const gridList = [
+  {
+    title: '2 columns',
+    column: 2,
+  },
+  {
+    title: '3 columns',
+    column: 3,
+  },
+  {
+    title: '4 columns',
+    column: 4,
+  },
+];
